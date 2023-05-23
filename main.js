60866ac44ecb97d017c9977c0a2a468dfe1aa2a6
ianpenalver <jabezpenalver2k20@gmail.com>
2023-05-23 23:06:17 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,276 @@
+// Imported Modules
+const { app, BrowserWindow, Menu, ipcMain } = require("electron");
+const path = require("path");
+const axios = require('axios');
+const dotenv = require('dotenv').config();
+const FormData = require('form-data');
+const fs = require('fs');
+
+// Global Variables
+const isDev = false;
+const isMac = process.platform === 'darwin';
+const template = [
+  // { role: 'appMenu' }
+  ...(isMac ? [{
+    label: app.name,
+    submenu: [
+      { role: 'about' },
+      { type: 'separator' },
+      { role: 'services' },
+      { type: 'separator' },
+      { role: 'hide' },
+      { role: 'hideOthers' },
+      { role: 'unhide' },
+      { type: 'separator' },
+      { role: 'quit' }
+    ]
+  }] : []),
+  // { role: 'fileMenu' }
+  {
+    label: 'File',
+    submenu: [
+      {
+        label: 'App Logs',
+        click: logsWindow
+      },
+      {
+        label: 'About',
+        click: aboutWindow
+      },
+      isMac ? { role: 'close' } : { role: 'quit' }
+    ]
+  },
+  // { role: 'editMenu' }
+  {
+    label: 'Edit',
+    submenu: [
+      { role: 'undo' },
+      { role: 'redo' },
+      { type: 'separator' },
+      { role: 'cut' },
+      { role: 'copy' },
+      { role: 'paste' },
+      ...(isMac ? [
+        { role: 'pasteAndMatchStyle' },
+        { role: 'delete' },
+        { role: 'selectAll' },
+        { type: 'separator' },
+        {
+          label: 'Speech',
+          submenu: [
+            { role: 'startSpeaking' },
+            { role: 'stopSpeaking' }
+          ]
+        }
+      ] : [
+        { role: 'delete' },
+        { type: 'separator' },
+        { role: 'selectAll' }
+      ])
+    ]
+  },
+  // { role: 'viewMenu' }
+  {
+    label: 'View',
+    submenu: [
+      { role: 'reload' },
+      { role: 'forceReload' },
+      { type: 'separator' },
+      { role: 'resetZoom' },
+      { role: 'zoomIn' },
+      { role: 'zoomOut' },
+      { type: 'separator' },
+      { role: 'minimize' },
+      { role: 'togglefullscreen' },
+    ]
+  }
+];
+
+// Main Window
+const createWindow = () => {
+  const main = new BrowserWindow({
+    width: isDev ? 1200 : 775,
+    height: 800,
+    resizable: false,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: true,
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  const menu = Menu.buildFromTemplate(template);
+  Menu.setApplicationMenu(menu);
+
+  if (isDev) {
+    main.webContents.openDevTools();
+  }
+
+  main.loadFile(path.join(__dirname, "./renderer/index.html"));
+};
+
+// Application Logs Window
+function logsWindow () {
+  const logs = new BrowserWindow({
+    width: 1200,
+    height: 500,
+    alwaysOnTop: true,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: true,
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  logs.setMenuBarVisibility(false);
+
+  if (isDev) {
+    logs.webContents.openDevTools();
+  }
+
+  logs.loadFile(path.join(__dirname, "./renderer/logs.html"));
+}
+
+// About Window
+function aboutWindow () {
+  const about = new BrowserWindow({
+    width: 400,
+    height: 400,
+    alwaysOnTop: true,
+  });
+
+  about.setMenuBarVisibility(false);
+
+  about.loadFile(path.join(__dirname, "./renderer/about.html"));
+}
+
+app.whenReady().then(() => {
+  // Initialize Functions
+  ipcMain.handle('axios.openAI', openAI);
+  ipcMain.handle('axios.tesseract', tesseract);
+  ipcMain.handle('axios.supaBase', supaBase);
+  ipcMain.handle('axios.backendLaravel', backendLaravel);
+
+  // Create Main Window
+  createWindow();
+
+  // Start Window
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+// Close Window
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+// Main Functions
+// Axios OpenAI API
+async function openAI(event, sentence, tools_type){
+  let result = null;
+
+  const env = dotenv.parsed;
+  await axios({
+      method: 'post',
+      url: 'https://api.openai.com/v1/completions',
+      data: {
+        model: "text-davinci-003",
+        prompt: ( tools_type == 'Grammar Correction' ? "Correct this to standard English:\n\n" : "Summarize this for a second-grade student:\n\n" ) +  sentence,
+        temperature: ( tools_type == 'Grammar Correction' ? 0 : 0.7 ),
+        max_tokens: ( tools_type == 'Grammar Correction' ? 60 : 64 ),
+        top_p: 1.0,
+        frequency_penalty: 0.0,
+        presence_penalty: 0.0
+      },
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + env.APIKEY_OPENAI
+      }
+    }).then(function (response) {
+      result = response.data;
+    })
+    .catch(function (error) {
+      result = error.response.data;
+    });
+
+  return result;
+}
+
+// Axios Tesseract API
+async function tesseract(event, filepath){
+  let result = null;
+
+  var formData = new FormData();
+  formData.append("image", fs.createReadStream(filepath));
+
+  await axios.post('http://backend.test/api/ocr', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    }).then(function (response) {
+      result = response.data;
+    })
+    .catch(function (error) {
+      result = error.response.data;
+    });
+
+  return result;
+}
+
+// Axios Supabase API
+async function supaBase(event, method, id = '', data = null){
+  let result = null;
+  const env = dotenv.parsed;
+
+  let query = ( method == 'get' ? '?select=*' : (method == 'delete' ? '?prompt_id=eq.' + id : '') );
+  await axios({
+      method: method,
+      url: 'https://lsuibxpvxqrxhkmxcmwy.supabase.co/rest/v1/prompts' + query,
+      headers: ( method == 'post' ? {
+          'apikey': env.APIKEY_SUPABASE,
+          'Authorization': 'Bearer ' + env.APIKEY_SUPABASE,
+          'Content-Type': 'application/json',
+          'Prefer': 'return=minimal'
+        } : {
+          'apikey': env.APIKEY_SUPABASE,
+          'Authorization': 'Bearer ' + env.APIKEY_SUPABASE 
+        } ),
+      data: ( method == 'post' ? data : null )
+    }).then(function (response) {
+      result = response.data;
+    })
+    .catch(function (error) {
+      result = error.response.data;
+    });
+
+  return result;
+}
+
+// Axios Laravel API
+async function backendLaravel(event, method, path, data = null, token = ''){
+  let result = null;
+
+  await axios({
+      method: method,
+      url: 'http://backend.test/api/' + path,
+      headers: ( token == '' ? { 
+            'Accept': 'application/json',
+        } : {
+            'Accept': 'application/json',
+            'Authorization': 'Bearer ' + token
+        } ),
+      data: data
+    }).then(function (response) {
+      result = response.data;
+    })
+    .catch(function (error) {
+      result = error.response.data;
+    });
+
+  return result;
+}
---
+++ renderer/js/logs.js
@@ -0,0 +1,149 @@
+// Form Login
+const form_login = document.getElementById("form_login");
+if (form_login) {
+    form_login.onsubmit = async function (e) {
+    e.preventDefault();
+
+    const btn_submit = document.querySelector("#form_login button[type='submit']");
+    const formData = new FormData(form_login);
+
+    btn_submit.innerHTML = '<span class="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span> Loading...';
+    btn_submit.disabled = true;
+
+    const response = await window.axios.backendLaravel('post', 'login', {
+            email: formData.get("email"),
+            password: formData.get("password"),
+        } );
+
+    // If email and password validation fails 
+    if ( response.user == null ) {
+        const field_email = document.querySelector("#form_login input[name='email']");
+        const field_password = document.querySelector("#form_login input[name='password']");
+        const invalid_email = document.getElementById("invalid_email");
+        const invalid_password = document.getElementById("invalid_password");
+
+        if ( response.errors.email == undefined ) {
+            invalid_email.innerHTML = '';
+            field_email.classList.remove('is-invalid');
+        }
+        else {
+            invalid_email.innerHTML = response.errors.email;
+            field_email.classList.add('is-invalid');
+        }
+        
+        if ( response.errors.password == undefined ) {
+            invalid_password.innerHTML = '';
+            field_password.classList.remove('is-invalid');
+        }
+        else {
+            invalid_password.innerHTML = response.errors.password;
+            field_password.classList.add('is-invalid');
+        }
+
+        btn_submit.innerHTML = 'Login';
+        btn_submit.disabled = false;
+        return;
+    }
+
+    // Store Token for Backend Laravel API access
+    sessionStorage.setItem('token', response.token);
+    alertMessage("success", "Successfully logged in account!");
+
+    // Hide Login Form and Show Prompts Table
+    const div_login = document.getElementById("div_login");
+    const div_prompts = document.getElementById("div_prompts");
+    div_login.classList.add('d-none');
+    div_prompts.classList.remove('d-none');
+    div_prompts.classList.add('d-flex');
+
+    btn_submit.innerHTML = 'Login';
+    btn_submit.disabled = false;
+
+    // Load Table
+    getPrompts();
+  };
+}
+
+// Btn Logout
+const btn_logout = document.getElementById('btn_logout');
+if (btn_logout) {
+    btn_logout.onclick = async function () {
+        btn_logout.innerHTML = '<span class="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span> Loading...';
+        btn_logout.disabled = true;
+
+        // Use Token to Logout
+        const token = sessionStorage.getItem('token');
+        const response = await window.axios.backendLaravel('post', 'logout', null, token);
+        console.log(response);
+
+        // Hide Login Form and Show Prompts Table
+        const div_login = document.getElementById("div_login");
+        const div_prompts = document.getElementById("div_prompts");
+        div_login.classList.remove('d-none');
+        div_login.classList.add('d-flex');
+        div_prompts.classList.remove('d-flex');
+        div_prompts.classList.add('d-none');
+
+        // Clear Login Form Fields
+        const field_email = document.querySelector("#form_login input[name='email']");
+        const field_password = document.querySelector("#form_login input[name='password']");
+        const invalid_email = document.getElementById("invalid_email");
+        const invalid_password = document.getElementById("invalid_password");
+        invalid_email.innerHTML = '';
+        field_email.value = '';
+        field_email.classList.remove('is-invalid');
+        invalid_password.innerHTML = '';
+        field_password.value = '';
+        field_password.classList.remove('is-invalid');
+        
+        btn_logout.innerHTML = 'Logout';
+        btn_logout.disabled = false;
+    }
+}
+
+// Read Prompts from SupaBase
+async function getPrompts () {
+    // Fetch API Response
+    const response = await window.axios.supaBase('get');
+
+    // Load table from API Response
+    let htmlResult = '';
+    Object.keys(response).forEach(key => {
+        let date = new Date(response[key].created_at.replace(' ', 'T'));
+
+        htmlResult += '<tr>' +
+            '<th scope="row">' +  response[key].prompt_id + '</th>' +
+            '<td>' + response[key].tools_type + '</td>' +
+            '<td>' + response[key].text + '</td>' +
+            '<td>' + response[key].result + '</td>' +
+            '<td>' + date.toLocaleString('en-US', { timeZone: 'UTC' }) + '</td>' +
+            '<td>' + 
+                '<div class="btn-group" role="group">' +
+                    '<button type="button" class="btn btn-primary btn-sm dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">' +
+                        'Action' +
+                    '</button>' +
+                    '<ul class="dropdown-menu">' +
+                        '<li><a id="btn_prompts_del" class="dropdown-item" href="#" name="' + response[key].prompt_id + '">Remove</a></li>' +
+                    '</ul>' +
+                '</div>' +
+        '</tr>';
+    });
+
+    const tbody = document.getElementById('tbl_prompts');
+    tbody.innerHTML = htmlResult;
+}
+
+// Set Btn Delete Prompt Click functionality from Table Prompts
+const tbl_prompts = document.getElementById('tbl_prompts');
+if (tbl_prompts) {
+    tbl_prompts.onclick = async function (e) {
+        if(e.target && e.target.id == "btn_prompts_del") {
+            const id = e.target.name;
+            const response = await window.axios.supaBase('delete', id);
+            console.log(response);
+            
+            alertMessage("success", "Successfully deleted id " + id + '!');
+            getPrompts();
+        }
+    };
+}
